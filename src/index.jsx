88bd9018eadcb85a98a38d4c4353d07a1d336187
mingwiki <mingwiki@gmail.com>
2025-06-13 22:25:37 +0800

    cool vscode prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,17 @@
-import { render } from 'preact';
-import { LocationProvider, Router, Route } from 'preact-iso';
-
-import { Header } from './components/Header.jsx';
-import { Home } from './pages/Home/index.jsx';
-import { NotFound } from './pages/_404.jsx';
-import './style.css';
-
+import { render } from "preact";
+import { LocationProvider, Router } from "preact-iso";
+import { Home } from "./pages/public";
 export function App() {
-	return (
-		<LocationProvider>
-			<Header />
-			<main>
-				<Router>
-					<Route path="/" component={Home} />
-					<Route default component={NotFound} />
-				</Router>
-			</main>
-		</LocationProvider>
-	);
+  return (
+    <LocationProvider>
+      <main>
+        <Router>
+          <Home path="/" />
+          <Home default />
+        </Router>
+      </main>
+    </LocationProvider>
+  );
 }
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById("app"));
